c23ddd5bb5016cd127981d2dfbc3725413209832
Nicholas Romero <nicholas_romero@brown.edu>
2025-03-11 23:50:07 -0400

    Implemented Number Buttons as well as basic operators

---
+++ myscript.js
@@ -0,0 +1,94 @@
+//Basic Operations
+let boolean = false;
+let numOne = '';
+let numTwo = '';
+let operator;
+
+const calculatorContent = document.querySelector('.display-box');
+
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+    if (numTwo == 0) {
+        return "Error";
+    }
+
+    return numOne/numTwo;
+}
+
+function exponent(numOne, numTwo) {
+    return numOne ** numTwo;
+}
+
+
+function operate(operator, numOne, numTwo) {
+    return operator(numOne, numTwo);
+}
+
+
+//make all number buttons functioning
+
+const buttons = document.querySelectorAll('[data-num]');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!boolean) {
+            numOne += button.getAttribute('data-num');
+            calculatorContent.textContent = numOne;
+        } else {
+            numTwo += button.getAttribute('data-num');
+            calculatorContent.textContent = numTwo;
+        }
+    })
+})
+
+
+const addButton = document.querySelector("#add")
+
+addButton.addEventListener('click', () => {
+    boolean = !boolean;
+    operator = add;
+
+})
+const minusButton = document.querySelector("#minus")
+
+minusButton.addEventListener('click', () => {
+    boolean = !boolean;
+    operator = subtract;
+
+})
+const multiplyButton = document.querySelector("#multiplication")
+
+multiplyButton.addEventListener('click', () => {
+    boolean = !boolean;
+    operator = multiply;
+
+})
+const divisionButton = document.querySelector("#division")
+
+divisionButton.addEventListener('click', () => {
+    boolean = !boolean;
+    operator = divide;
+
+})
+
+
+const equalButton = document.querySelector("#equal");
+
+equalButton.addEventListener('click', () => {
+    
+})
+
+
+
+
